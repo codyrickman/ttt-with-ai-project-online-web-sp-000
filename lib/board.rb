2e5438b856fd7563346177204701f3be9c5d380b
Cody Rickman <cody@creativemindstudios.com>
2019-08-13 00:39:58 +0000

    Automatically backed up by Learn

---
+++ lib/board.rb
@@ -0,0 +1,13 @@
+class board
+  attr_accessor :board
+  def initialize
+    @board = ["", "", "", "", "", "", "", "", ""]
+  end
+  def cells ()
+
+  end
+  def reset!
+    @board = ["", "", "", "", "", "", "", "", ""]
+
+  end
+end
